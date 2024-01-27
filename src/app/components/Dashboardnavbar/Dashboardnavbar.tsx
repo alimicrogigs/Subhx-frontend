5e094b2d9fd3032feabbdae5ac0cab0b5fa9afa8
saadsubhx <saad.tech@subhx.in>
2024-01-27 23:15:59 +0530

    changes in menu bar
    changes in menu bar

--- src/app/components/Dashboardnavbar/Dashboardnavbar.tsx
+++ src/app/components/Dashboardnavbar/Dashboardnavbar.tsx
@@ -225,7 +225,7 @@ export default function () {
                   backgroundRepeat: "no-repeat",
                   backgroundSize: "contain",
                 }}
-                className="w-[30px] h-[40px]"
+                className="w-[25px] h-[30px]"
               ></div>
               <p> jason</p>
               <div
@@ -247,13 +247,13 @@ export default function () {
                   animate={{ opacity: 1, y: 0 }}
                   exit={{ opacity: 0, y: -20 }}
                   transition={{ duration: 0.1 }}
-                  className="  bg-[#07303F] rounded-bl-[10px] rounded-br-[10px]"
+                  className="  bg-[#07303F] rounded-bl-[10px] rounded-br-[10px] px-[30px]"
                 >
                   {/* ....... first menu ........ */}
                   <Link href="/dashboard/account-setting">
                     <div
                       onClick={closeallmenu}
-                      className="flex gap-[20px] py-[10px] text-[1.2rem] items-center pl-[10px] "
+                      className="flex gap-[20px] py-[20px] text-[1.2rem] items-center pl-[10px] "
                     >
                       <div
                         style={{
@@ -267,7 +267,7 @@ export default function () {
                   </Link>
                   {/* ..... second item ..... */}
                   <Link href="/dashboard/account-setting">
-                    <div className="flex gap-[20px] py-[10px] text-[1.2rem] items-center pl-[10px]">
+                    <div className="flex gap-[20px] py-[20px] text-[1.2rem] items-center pl-[10px]">
                       <div
                         style={{
                           backgroundImage:
@@ -280,7 +280,7 @@ export default function () {
                   </Link>
                   {/* ......... third item .......... */}
                   <Link href="/dashboard/account-setting">
-                    <div className="flex gap-[20px] py-[10px] text-[1.2rem] items-center pl-[10px]">
+                    <div className="flex gap-[20px] py-[20px] text-[1.2rem] items-center pl-[10px]">
                       <div
                         style={{
                           backgroundImage:
