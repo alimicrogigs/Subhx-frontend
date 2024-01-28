a30581e2e60d17dcc4e54da812781c0864201865
MasoomShubx <157352744+MasoomShubx@users.noreply.github.com>
2024-01-28 18:36:06 +0530

    Added helper.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import dotenv from 'dotenv';
+
+dotenv.config();
+const nextConfig = {
+    env: {
+        API_URL: 'http://192.168.1.6:8000/api/v1/',
+      },
+};
+
 
 export default nextConfig;
--- src/app/utils/lib/requestHelpers.js
+++ src/app/utils/lib/requestHelpers.js
@@ -1,31 +1,55 @@
-exports.postRequestAPIHelper = async (url, authorizationToken, requestData) =>{
-    try {
-      const config = {
-        method: 'post',
-        maxBodyLength: Infinity,
-        url: url,
-        headers: { 
-          'Content-Type': 'application/json', 
-          'Authorization': authorizationToken
-        },
-        data: JSON.stringify(requestData)
-      };
+const axios = require('axios');
+const UAParser = require('ua-parser-js');
+
+const getUserAgentInfo = () => {
+  const userAgent = window.navigator.userAgent;
+  const parser = new UAParser();
+  const result = parser.setUA(userAgent).getResult();
+
+  // Extracted information
+  const browserName = result.browser.name;
+  const browserVersion = result.browser.version;
+  const osName = result.os.name;
+  const osVersion = result.os.version;
+
+  // Create a custom user agent string
+  const customUserAgent = `${browserName} ${browserVersion}; ${osName} ${osVersion}`;
   
-      const response = await axios.request(config);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-}
+  return customUserAgent;
+};
+
+exports.postRequestAPIHelper = async (url, authorizationToken, requestData) => {
+  try {
+    const config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': authorizationToken,
+        'User-Agent': getUserAgentInfo(),
+      },
+      data: JSON.stringify(requestData)
+    };
+
+    const response = await axios.request(config);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
 
 
-exports.getRequestAPIHelper = async (url="", authorizationToken="") => {
+exports.getRequestAPIHelper = async (url, authorizationToken) => {
+  const userAgentInfo = getUserAgentInfo();
     try {
       const config = {
         method: 'get',
         url: url,
         headers: { 
-          'Authorization': authorizationToken
+          'Content-Type': 'application/json', 
+          'Authorization': authorizationToken,
+          'User-Agent': JSON.stringify(userAgentInfo),
         }
       };
   
@@ -34,6 +58,6 @@ exports.getRequestAPIHelper = async (url="", authorizationToken="") => {
     } catch (error) {
       throw error;
     }
-  };
+};
 
 
