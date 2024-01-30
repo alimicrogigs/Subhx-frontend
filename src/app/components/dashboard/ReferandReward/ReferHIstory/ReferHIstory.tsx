002fcdd5eba53e1b1df3ffeeda12d779873d144c
saadsubhx <saad.tech@subhx.in>
2024-01-30 21:46:35 +0530

    complete refer and reward page

--- src/app/components/dashboard/ReferandReward/ReferHIstory/ReferHIstory.tsx
+++ src/app/components/dashboard/ReferandReward/ReferHIstory/ReferHIstory.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 
 export default function Referfriend() {
+  const userData = [
+    { email: "user1@example.com", date: "2022-02-01", reward: "$50" },
+    { email: "user2@example.com", date: "2022-02-05", reward: "$30" },
+    { email: "user3@example.com", date: "2022-02-10", reward: "$25" },
+    { email: "user4@example.com", date: "2022-02-15", reward: "$40" },
+    { email: "user5@example.com", date: "2022-02-20", reward: "$55" },
+    { email: "user6@example.com", date: "2022-02-25", reward: "$60" },
+    { email: "user7@example.com", date: "2022-03-01", reward: "$45" },
+    { email: "user8@example.com", date: "2022-03-05", reward: "$35" },
+    { email: "user9@example.com", date: "2022-03-10", reward: "$30" },
+    { email: "user10@example.com", date: "2022-03-15", reward: "$20" },
+  ];
+
   return (
     <>
-      <div className="w-[100%]">
+      <div className="w-[100%] sm:text-[.8rem] text-[.6rem]">
         <table className="w-[100%]">
           <thead>
             <tr className="bg-[#032835] ">
@@ -13,16 +26,15 @@ export default function Referfriend() {
             </tr>
           </thead>
           <tbody>
-            <tr className="border-b border-b-[#032835]">
-              <td className="py-[20px]">user1@example.com</td>
-              <td className="py-[20px]">2022-02-01</td>
-              <td className="py-[20px]">$50</td>
-            </tr>
-            <tr>
-              <td className="py-[20px]">user2@example.com</td>
-              <td className="py-[20px]">2022-02-05</td>
-              <td className="py-[20px]">$30</td>
-            </tr>
+            {userData.map((user, index) => (
+              <tr key={index} className="border-b border-b-[#032835]">
+                <td className="py-[20px]">{user.email}</td>
+                <td className="py-[20px]">{user.date}</td>
+                <td className="py-[20px] sm:text-left text-center">
+                  {user.reward}
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
